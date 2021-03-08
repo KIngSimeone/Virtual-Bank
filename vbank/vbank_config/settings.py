bc0e8ad2ad0de45e0dfcf1575e6a1024ed897810
Kingsimeone <simeoneumoh@outlook.com>
2021-03-08 09:44:24 +0100

    env file and settings added

--- vbank/vbank_config/settings.py
+++ vbank/vbank_config/settings.py
@@ -17,12 +17,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ENV_PATH = BASE_DIR / '.env'
+load_dotenv(ENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uuwd_+%pi7p0on74d9v83l7s=e-fy$m&b@26hh&4f2cymxxrzn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
