96c4fe09418fadcfb2f8cc4a33f987b60a7c5905
Kingsimeone <simeoneumoh@outlook.com>
2021-03-08 09:40:01 +0100

    neccessary imporst settings"

--- vbank/vbank_config/settings.py
+++ vbank/vbank_config/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from corsheaders.defaults import default_headers
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
